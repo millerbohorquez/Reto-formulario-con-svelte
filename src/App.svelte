6c82c48bf50c55bbecaf9a8c7f19b3a97db43e0a
millerbohorquez <miller-1107@hotmail.com>
2024-08-19 18:52:00 -0500

    comentarios

--- src/App.svelte
+++ src/App.svelte
@@ -1,136 +1,230 @@
-
 <script>
-    import { createForm } from "svelte-forms-lib";
+  import { onMount } from "svelte";  
 
-	let showMessage = false;
-
-    const { form, errors, handleChange, handleSubmit } = createForm({
-      initialValues: {
-        
-		first_name:"",
-		last_name:"",
-		email_address: "",
-		query_type:"",
-		message:"",
-		consent:"",
+  let data = {
+    currentUser: {
+      image: {
+        png: "./images/avatars/image-juliusomo.png",
+        webp: "./images/avatars/image-juliusomo.webp",
       },
-      validate: values => {
-        let errs = {};
-		if (values.first_name === "") {
-          errs["first_name"] = "This field is required";
-        }
-		if (values.last_name === "") {
-          errs["last_name"] = "This field is required";
-        }
-		if (values.email_address === "") {
-			errs["email_address"] = "Please enter a valid email address";
-		}
-		if (values.query_type === "") {
-          errs["query_type"] = "Please select a query type";
-        }
-		if (values.message === "") {
-			errs["message"] = "This field is required";
-		}
-		if (values.consent === "") {
-			errs["consent"] = "To submit this form, please consent to being contacted";
-		}
-        return errs;
+      username: "juliusomo",
+    },
+    comments: [
+      {
+        parent: 0,
+        id: 1,
+        content:
+          "Impressive! Though it seems the drag feature could be improved. But overall it looks incredible. You've nailed the design and the responsiveness at various breakpoints works really well.",
+        createdAt: "1 month ago",
+        score: 12,
+        user: {
+          image: {
+            png: "./images/avatars/image-amyrobson.png",
+            webp: "./images/avatars/image-amyrobson.webp",
+          },
+          username: "amyrobson",
+        },
+        replies: [],
       },
-      onSubmit: () => {
-        showMessage = true; 
-        setTimeout(() => {
-          showMessage = false; 
-        }, 3000);
-      }
-    });
+      {
+        parent: 0,
+        id: 2,
+        content:
+          "Woah, your project looks awesome! How long have you been coding for? I'm still new, but think I want to dive into React as well soon. Perhaps you can give me an insight on where I can learn React? Thanks!",
+        createdAt: "2 weeks ago",
+        score: 5,
+        user: {
+          image: {
+            png: "./images/avatars/image-maxblagun.png",
+            webp: "./images/avatars/image-maxblagun.webp",
+          },
+          username: "maxblagun",
+        },
+        replies: [
+          {
+            parent: 2,
+            id: 1,
+            content:
+              "If you're still new, I'd recommend focusing on the fundamentals of HTML, CSS, and JS before considering React. It's very tempting to jump ahead but lay a solid foundation first.",
+            createdAt: "1 week ago",
+            score: 4,
+            replyingTo: "maxblagun",
+            user: {
+              image: {
+                png: "./images/avatars/image-ramsesmiron.png",
+                webp: "./images/avatars/image-ramsesmiron.webp",
+              },
+              username: "ramsesmiron",
+            },
+          },
+          {
+            parent: 2,
+            id: 2,
+            content:
+              "I couldn't agree more with this. Everything moves so fast and it always seems like everyone knows the newest library/framework. But the fundamentals are what stay constant.",
+            createdAt: "2 days ago",
+            score: 2,
+            replyingTo: "ramsesmiron",
+            user: {
+              image: {
+                png: "image/",
+                webp: "images/avatars/image-juliusomo.webp",
+              },
+              username: "juliusomo",
+            },
+          },
+        ],
+      },
+    ],
+  };
 
-  </script>
+  let newComment = ''; 
 
-<header>
-	<div id="confirmationMessage"  style="{showMessage ? 'display: block;' : 'display: none;'}">
-		<p>
-			<svg xmlns="http://www.w3.org/2000/svg" width="20" height="21" fill="none" viewBox="0 0 20 21">
-				<path fill="#fff"
-					d="M14.28 7.72a.748.748 0 0 1 0 1.06l-5.25 5.25a.748.748 0 0 1-1.06 0l-2.25-2.25a.75.75 0 1 1 1.06-1.06l1.72 1.72 4.72-4.72a.75.75 0 0 1 1.06 0Zm5.47 2.78A9.75 9.75 0 1 1 10 .75a9.76 9.76 0 0 1 9.75 9.75Zm-1.5 0A8.25 8.25 0 1 0 10 18.75a8.26 8.26 0 0 0 8.25-8.25Z" />
-			</svg>Message Sent!
-		</p>
-		<p>Thanks for completing the form. We'll be in touch soon!</p>
-	</div>
-</header>
-<main>
-	<div class="form-container">
-		<form id="contactForm" on:submit={handleSubmit}>
-			<p class="negrita">Contact Us</p>
+function addComment(body, parentId = 0, replyTo = undefined) {
+  let commentParent =
+    parentId === 0
+      ? data.comments
+      : data.comments.find((c) => c.id == parentId).replies;
+  let newComment = {
+    parent: parentId,
+    id: commentParent.length == 0 ? 1 : commentParent[commentParent.length - 1].id + 1,
+    content: body,
+    createdAt: "Now",
+    replyingTo: replyTo,
+    score: 0,
+    replies: parentId == 0 ? [] : undefined,
+    user: data.currentUser,
+  };
+  commentParent.push(newComment);
+  data = { ...data };
+}
 
-			<div class="name-fields">
-				<label for="first_name">First Name <span>*</span>
-					<input type="text" id="first_name" name="first_name" class="first_name" on:change={handleChange} bind:value={$form.first_name}>
-					{#if $errors.first_name}
-      				<small>{$errors.first_name}</small>
-    				{/if}
-				</label>
+function deleteComment(commentObject) {
+  if (commentObject.parent == 0) {
+    data.comments = data.comments.filter((e) => e != commentObject);
+  } else {
+    let parentComment = data.comments.find((e) => e.id === commentObject.parent);
+    parentComment.replies = parentComment.replies.filter((e) => e != commentObject);
+  }
+  data = { ...data };
+}
 
-				<label for="last_name">Last Name <span>*</span>
-					<input type="text" id="last_name" name="last_name" class="last_name" on:change={handleChange} bind:value={$form.last_name}>
-					{#if $errors.last_name}
-      				<small>{$errors.last_name}</small>
-    				{/if}
-				</label>
-			</div>
+function promptDel(commentObject) {
+  let modalWrp = document.querySelector(".modal-wrp");
+  modalWrp.classList.remove("invisible");
+  modalWrp.querySelector(".yes").addEventListener("click", () => {
+    deleteComment(commentObject);
+    modalWrp.classList.add("invisible");
+  });
+  modalWrp.querySelector(".no").addEventListener("click", () => {
+    modalWrp.classList.add("invisible");
+  });
+}
 
-			<label for="email_address">Email Address <span>*</span></label>
-			<input type="email" id="email_address" name="email_address" class="email_address" on:change={handleChange} bind:value={$form.email_address}>
-			{#if $errors.email_address}
-			  <small>{$errors.email_address}</small><br>
-			{/if}
+function spawnReplyInput(parentId, replyTo = undefined) {
+  let commentBody = prompt("Enter your reply:");
+  if (commentBody.length > 0) {
+    addComment(commentBody, parentId, replyTo);
+  }
+}
 
-			<br>
-			
-			<label for="query_type">Query Type <span>*</span></label>
-			<div class="radio-group">
-				<label class="l1">
-					<input type="radio" class="r1" name="query_type" value="general" on:change={handleChange} bind:group={$form.query_type}>
-					<svg xmlns="http://www.w3.org/2000/svg" width="20" height="21" fill="none" viewBox="0 0 20 21">
-						<path class="svg" fill="#0C7D69"
-							d="M10 .75a9.75 9.75 0 1 0 9.75 9.75A9.76 9.76 0 0 0 10 .75Zm0 18a8.25 8.25 0 1 1 8.25-8.25A8.26 8.26 0 0 1 10 18.75Zm5.25-8.25a5.25 5.25 0 1 1-10.499 0 5.25 5.25 0 0 1 10.499 0Z" />
-					</svg>
-					General Enquiry
-				</label>
-				<label class="l2">
-					<input type="radio" name="query_type" value="support" on:change={handleChange} bind:group={$form.query_type}>
-					<svg xmlns="http://www.w3.org/2000/svg" width="20" height="21" fill="none" viewBox="0 0 20 21">
-						<path class="svg" fill="#0C7D69"
-							d="M10 .75a9.75 9.75 0 1 0 9.75 9.75A9.76 9.76 0 0 0 10 .75Zm0 18a8.25 8.25 0 1 1 8.25-8.25A8.26 8.26 0 0 1 10 18.75Zm5.25-8.25a5.25 5.25 0 1 1-10.499 0 5.25 5.25 0 0 1 10.499 0Z" />
-					</svg>
-					Support Request
-				</label>
-			</div>
-			{#if $errors.query_type}
-                <small>{$errors.query_type}</small><br>
-            {/if}
 
-			<label for="message">Message <span>*</span></label>
-			<textarea id="message" name="message" class="message" rows="4" on:change={handleChange} bind:value={$form.message}></textarea>
-			{#if $errors.message}
-			  <small>{$errors.message}</small><br>
-			{/if}
-			<br>
-			<div class="checkbox-group">
-				<label>
-					<input type="checkbox" name="consent" on:change={handleChange} bind:value={$form.consent}>
-					<svg xmlns="http://www.w3.org/2000/svg" width="15" height="15" fill="none" viewBox="0 0 18 18">
-						<path class="svg" fill="#0C7D69"
-							d="M16.5 0h-15A1.5 1.5 0 0 0 0 1.5v15A1.5 1.5 0 0 0 1.5 18h15a1.5 1.5 0 0 0 1.5-1.5v-15A1.5 1.5 0 0 0 16.5 0Zm-3.22 7.28-5.25 5.25a.748.748 0 0 1-1.06 0l-2.25-2.25a.75.75 0 1 1 1.06-1.06l1.72 1.72 4.72-4.72a.751.751 0 0 1 1.06 1.06Z" />
-					</svg>
-					I consent to being contacted by the team <span>*</span>
-				</label>
-				{#if $errors.consent}
-			  		<br><small>{$errors.consent}</small>
-				{/if}
-			</div>
+function scorePlus(commentObject) {
+  commentObject.score++;
+  data = { ...data };
+}
 
-			<button type="submit" class="ver">Submit</button>
-		</form>
-	</div>
+function scoreMinus(commentObject) {
+  if (commentObject.score > 0) {
+    commentObject.score--;
+    data = { ...data };
+  }
+}
+</script>
+
+
+<main class="container">
+  <div class="comment-section ">
+    <div class="comments-wrp ">
+      {#each data.comments as comment}
+        <div class="comment-wrp ">
+          <div class="comment">
+            <div class="c-score">
+              <svg class="score-control" on:click={() => scorePlus(comment)} width="11" height="11" xmlns="http://www.w3.org/2000/svg"><path d="M6.33 10.896c.137 0 .255-.05.354-.149.1-.1.149-.217.149-.354V7.004h3.315c.136 0 .254-.05.354-.149.099-.1.148-.217.148-.354V5.272a.483.483 0 0 0-.148-.354.483.483 0 0 0-.354-.149H6.833V1.4a.483.483 0 0 0-.149-.354.483.483 0 0 0-.354-.149H4.915a.483.483 0 0 0-.354.149c-.1.1-.149.217-.149.354v3.37H1.08a.483.483 0 0 0-.354.15c-.1.099-.149.217-.149.353v1.23c0 .136.05.254.149.353.1.1.217.149.354.149h3.333v3.39c0 .136.05.254.15.353.098.1.216.149.353.149H6.33Z" fill="#C5C6EF"/></svg>
+              <p class="score-number font-medium">{comment.score}</p>
+              <svg class="score-control" on:click={() => scoreMinus(comment)} width="11" height="3" xmlns="http://www.w3.org/2000/svg"><path d="M9.256 2.66c.204 0 .38-.056.53-.167.148-.11.222-.243.222-.396V.722c0-.152-.074-.284-.223-.395a.859.859 0 0 0-.53-.167H.76a.859.859 0 0 0-.53.167C.083.437.009.57.009.722v1.375c0 .153.074.285.223.396a.859.859 0 0 0 .53.167h8.495Z" fill="#C5C6EF"/></svg>
+            </div>
+            <div class="c-user">
+              <img src={comment.user.image.webp} alt="" class="usr-img">
+              <p class="usr-name">{comment.user.username}</p>
+              <p class="cmnt-at">{comment.createdAt}</p>
+            </div>
+            <div class="c-controls">
+              {#if comment.user.username == data.currentUser.username}
+              <a class="c-delete " on:click={() => promptDel(comment)}>
+                <svg width="12" height="14" xmlns="http://www.w3.org/2000/svg"><path d="M1.167 12.448c0 .854.7 1.552 1.555 1.552h6.222c.856 0 1.556-.698 1.556-1.552V3.5H1.167v8.948Zm10.5-11.281H8.75L7.773 0h-3.88l-.976 1.167H0v1.166h11.667V1.167Z" fill="#ED6368"/></svg>                <span>Delete</span>
+              </a>
+                <a class="c-edit">
+                  <svg width="14" height="14" xmlns="http://www.w3.org/2000/svg"><path d="M13.479 2.872 11.08.474a1.75 1.75 0 0 0-2.327-.06L.879 8.287a1.75 1.75 0 0 0-.5 1.06l-.375 3.648a.875.875 0 0 0 .875.954h.078l3.65-.333c.399-.04.773-.216 1.058-.499l7.875-7.875a1.68 1.68 0 0 0-.061-2.371Zm-2.975 2.923L8.159 3.449 9.865 1.7l2.389 2.39-1.75 1.706Z" fill="#5357B6"/></svg>
+                  <span>Edit</span>
+                </a>
+              {/if}
+              <a class="c-reply" on:click={() => spawnReplyInput(comment.id)}>
+                <svg width="14" height="13" xmlns="http://www.w3.org/2000/svg"><path d="M.227 4.316 5.04.16a.657.657 0 0 1 1.085.497v2.189c4.392.05 7.875.93 7.875 5.093 0 1.68-1.082 3.344-2.279 4.214-.373.272-.905-.07-.767-.51 1.24-3.964-.588-5.017-4.829-5.078v2.404c0 .566-.664.86-1.085.496L.227 5.31a.657.657 0 0 1 0-.993Z" fill="#5357B6"/></svg>
+                <span>Reply</span>
+              </a>
+            </div>
+            <p class="c-text ">
+              {#if comment.replyingTo}
+                <span class="reply-to">@{comment.replyingTo}</span>
+              {/if}
+              <span class="c-body">{comment.content}</span>
+            </p>
+          </div>
+          {#if comment.replies.length > 0}
+            <div class="replies comments-wrp ">
+              {#each comment.replies as reply}
+                <div class="comment">
+                  <!-- Repite la estructura del comentario aquí para las respuestas -->
+                  <div class="c-user">
+                    <img src={reply.user.image.webp} alt="" class="usr-img">
+                    <p class="usr-name">{reply.user.username}</p>
+                    <p class="cmnt-at">{reply.createdAt}</p>
+                  </div>
+                  <p class="c-text">
+                    {#if reply.replyingTo}
+                      <span class="reply-to">@{reply.replyingTo}</span>
+                    {/if}
+                    <span class="c-body">{reply.content}</span>
+                  </p>
+                </div>
+              {/each}
+            </div>
+          {/if}
+        </div>
+      {/each}
+    </div>
+
+    <div class="reply-input">
+      <img src={data.currentUser.image.webp} alt="" class="usr-img">
+      <textarea class="cmnt-input" placeholder="Add a comment..." bind:value={newComment}></textarea>
+      <button class="bu-primary" on:click={() => {
+        if (newComment.length > 0) {
+          addComment(newComment);
+          newComment = '';
+        }
+      }}>SEND</button>
+    </div>
+  </div>
+
+  <div class="modal-wrp invisible">
+    <div class="modal container">
+      <h3 class="">Delete comment</h3>
+      <p class="">Are you sure you want to delete this comment? This will remove the comment and can't be undone.</p>
+      <div class="flex justify-between">
+        <button class="no">NO, CANCEL</button>
+        <button class="yes">YES, DELETE</button>
+      </div>
+    </div>
+  </div>
 </main>
-
